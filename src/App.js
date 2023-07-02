ac206ee04ef40c8d2378773d78c64d038aa729b9
Yohanna <rebecca.yohanna@gmail.com>
2023-07-02 21:45:11 +0700

    [DEV] Modify app.js files to show Navbar and Market page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Market from './components/Market';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto flex flex-col gap-4 lg:w-10/12 sm:w-screen px-5">
+      <Navbar />
+      <Market />
     </div>
   );
 }
