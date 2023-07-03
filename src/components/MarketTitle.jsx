a64b0421a0554c6d1a8379e39263160d92128a68
Yohanna <rebecca.yohanna@gmail.com>
2023-07-03 22:51:01 +0700

    [DEV] Add Dummy Search List

--- src/components/MarketTitle.jsx
+++ src/components/MarketTitle.jsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 import SearchBar from "./Searchbar/SearchBar";
 
 const MarketTitle = () => {
-    // const { data: dataSupportedCurrencies } = useSupportedCurrencies();
-    const [searchSuggestions, setSearchSuggestions] = useState([]);
+    const [searchSuggestions, setSearchSuggestions] = useState([
+        {
+            color: "#F78B1A",
+            currencyGroup: "BTC",
+            currencySymbol: "BTC",
+            logo: "https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_BTC.svg",
+            name: "Bitcoin",
+        }
+    ]);
 
-    // useEffect(() => {
-    //     console.log('dataSupportedCurrencies ', dataSupportedCurrencies);
-    // }, [dataSupportedCurrencies])
-
-    return(
+    return (
         <div className="flex flex-row justify-between">
             <div className="sm:text-2xl text-s font-bold">Harga Crypto dalam Rupiah Hari Ini</div>
-            <SearchBar suggestions={searchSuggestions}/>
+            <SearchBar suggestions={searchSuggestions} />
         </div>
     );
 }
