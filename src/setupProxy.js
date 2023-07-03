5cbbee19d92a6e6cf327adbeb2b79d6bdcebbe63
Yohanna <rebecca.yohanna@gmail.com>
2023-07-03 14:49:04 +0700

    [DEV] Add vercel.json, change setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,43 +1,44 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://api.pintu.co.id',
-      changeOrigin: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Cache-Control': 'no-store, no-cache, must-revalidate',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-        // Pragma header with the value no-cache and the Expires header with the value 0. These headers provide additional instructions to prevent caching.
-      },
-      pathRewrite: {
-        '^/api': '',
-      },
-    })
-  );
+// module.exports = function (app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'https://api.pintu.co.id',
+//       changeOrigin: true,
+//       headers: {
+//         'Access-Control-Allow-Origin': '*',
+//         'Cache-Control': 'no-store, no-cache, must-revalidate',
+//         'Pragma': 'no-cache',
+//         'Expires': '0',
+//         // Pragma header with the value no-cache and the Expires header with the value 0. These headers provide additional instructions to prevent caching.
+//       },
+//       pathRewrite: {
+//         '^/api' : '/api',
+//         '^/api/*' : "/api/*"
+//       }
+//     })
+//   );
   
-  app.use(
-    '/static.pintu.co.id/assets/images/logo',
-    createProxyMiddleware({
-      target: 'https://s3-ap-southeast-1.amazonaws.com',
-      changeOrigin: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
-  );
+//   app.use(
+//     '/static.pintu.co.id/assets/images/logo',
+//     createProxyMiddleware({
+//       target: 'https://s3-ap-southeast-1.amazonaws.com',
+//       changeOrigin: true,
+//       headers: {
+//         'Access-Control-Allow-Origin': '*',
+//       },
+//     })
+//   );
 
-  app.use(
-    '/content.pintu.co.id',
-    createProxyMiddleware({
-      target: 'https://s3-ap-southeast-1.amazonaws.com',
-      changeOrigin: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
-  );
-};
+//   app.use(
+//     '/content.pintu.co.id',
+//     createProxyMiddleware({
+//       target: 'https://s3-ap-southeast-1.amazonaws.com',
+//       changeOrigin: true,
+//       headers: {
+//         'Access-Control-Allow-Origin': '*',
+//       },
+//     })
+//   );
+// };
