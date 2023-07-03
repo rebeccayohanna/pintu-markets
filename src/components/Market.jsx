d5aa977e7d0b461c8b4ac7357b4e25177bdd98d2
Yohanna <rebecca.yohanna@gmail.com>
2023-07-03 23:06:15 +0700

    [DEV] Add url link on tags

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -3,56 +3,56 @@ import MarketTable from "./MarketTable";
 import MarketTitle from "./MarketTitle";
 
 const Market = () => {
-    
+
     const tagLists = [
         {
             title: 'Terbaru',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/new',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Latest_b83e6c1ad1.svg",
         },
         {
             title: 'DeFi',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/defi',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/De_Fi_c2cbe56025.svg",
         },
         {
             title: 'NFT/Gaming',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/gaming',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Gaming_0d52ecf73a.svg",
         },
         {
             title: 'CEX',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/cex',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Cex_33897131c9.svg",
         },
         {
             title: 'DEX',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/dex',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Dex_309b635c4c.svg",
         },
         {
             title: 'Layer-1',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/layer-1',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Layer1_f326cdc673.svg",
         },
         {
             title: 'Infrastructure',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/infrastructure',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Infra_1ff3846c0a.svg",
         },
         {
             title: 'Lending',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/lending',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Lending_f45b671bb7.svg",
         },
         {
             title: 'Layer-2',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/layer-2',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Layer2_aa0d8bfe6d.svg",
         },
         {
             title: 'Ekosistem Stablecoin',
-            href: '/',
+            href: 'https://pintu.co.id/market/tags/stablecoin',
             icon: "https://s3.ap-southeast-1.amazonaws.com/content.pintu.co.id/Stablecoin_666453c616.svg",
         },
     ]
@@ -60,7 +60,7 @@ const Market = () => {
     return (
         <div className="flex flex-col gap-4">
             <MarketTitle />
-            <GroupTags tagLists={tagLists}/>
+            <GroupTags tagLists={tagLists} />
             <MarketTable />
         </div>
     );
--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,10 +1,15 @@
 import { ReactSVG } from 'react-svg';
 
 const Tags = ({ title, icon, link }) => {
+
+    const onLinkClick = (href) => {
+        window.open(href, '_blank');
+    }
+
     return (
-        <div className="flex">
+        <div className="flex" onClick={() => onLinkClick(link)}>
             <div className="flex rounded-lg p-1 gap-2 items-center bg-blue-100 text-xs text-blue-700 font-semibold">
-                <ReactSVG src={icon.replace('https://s3-ap-southeast-1.amazonaws.com', '')} style={{ color: '#5D6FDA' }}/>
+                <ReactSVG src={icon.replace('https://s3-ap-southeast-1.amazonaws.com', '')} style={{ color: '#5D6FDA' }} />
                 {title}
             </div>
         </div>
