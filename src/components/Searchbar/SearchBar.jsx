64d380e1d54dd938b6fc5bbc88d390f1e521bac4
Yohanna <rebecca.yohanna@gmail.com>
2023-07-03 22:52:45 +0700

    [DEV] Add Dummy Search List (2)

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
-import {ReactComponent as SearchLogo} from '../../assets/icons/search.svg';
+import { ReactComponent as SearchLogo } from '../../assets/icons/search.svg';
+import { ReactSVG } from 'react-svg';
 
-function SearchBar({suggestions}) {
+function SearchBar({ suggestions }) {
     const [searchTerm, setSearchTerm] = useState('');
     const [showSuggestions, setShowSuggestions] = useState(false);
     const searchRef = useRef(null);
@@ -46,17 +47,23 @@ function SearchBar({suggestions}) {
             />
             {showSuggestions && (
                 <ul className="absolute z-10 mt-2 w-64 bg-white border border-gray-300 rounded-md shadow-md">
-                    {suggestions
-                        .filter((suggestion) => suggestion.toLowerCase().includes(searchTerm.toLowerCase()))
-                        .map((suggestion) => (
-                            <li
-                                key={suggestion}
-                                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-                                onClick={() => handleSuggestionClick(suggestion)}
-                            >
-                                {suggestion}
-                            </li>
-                        ))}
+                    {suggestions?.map((suggestion) => (
+                        <li
+                            key={suggestion.name}
+                            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                            onClick={() => handleSuggestionClick(suggestion.name)}
+                        >
+                            <div className="flex items-center gap-6">
+                                <div className="flex items-center ml-1">
+                                    <ReactSVG src={suggestion?.logo.replace('https://s3-ap-southeast-1.amazonaws.com', '')} style={{ color: suggestion.color }} />
+                                </div>
+                                <div className="flex flex-1 flex-wrap">
+                                    <p className="flex-1 text-left">{suggestion?.name}</p>
+                                    <p className="mr-6 w-14 text-gray-500">{suggestion?.currencyGroup}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             )}
         </div>
